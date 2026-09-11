9f564d5e27ad6a0397a3d7bc93c9ba2089c7d3e5
coco-papiyon <coco-papiyon@coco.ne.jp>
2026-09-11 23:37:23 +0900

    起動シェル修正

--- upper-start.bat
+++ upper-start.bat
@@ -8,7 +8,7 @@ REM Usage: upper-start.bat [port] [relay-port]
 cd /d "%~dp0"
 
 set "PORT=%~1"
-if "%PORT%"=="" set "PORT=3100"
+if "%PORT%"=="" set "PORT=3200"
 
 set "RELAY_PORT=%~2"
 if "%RELAY_PORT%"=="" set /a "RELAY_PORT=PORT+1"
